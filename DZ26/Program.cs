38761fae93d5bc0c097f468bd5078506e40d4a3d
Olga <olg3001@yandex.ru>
2022-11-14 00:54:33 +0300

    DZ26_Hard

---
+++ DZ26/Program.cs
@@ -0,0 +1,50 @@
+﻿// Напишите программу, которая принимает на вход целое или дробное число 
+// и выдаёт количество цифр в числе.
+// 452 -> 3
+// 82 -> 2
+// 9,012 ->4
+
+Console.WriteLine("Program to get the digits number of the inputed number.");
+Console.Write("Input number:\t");
+decimal number = decimal.Parse(Console.ReadLine()!);
+
+if (number == 0)
+{
+    Console.WriteLine("There is 1 digit");
+}
+
+else
+{
+    while (number < 0)
+    {
+        number = number * -1;
+    }
+
+    while (number % 1 > 0)
+    {
+        number *= 10;
+    }
+
+    int newNumber = (int)number;
+
+    int GetNumberOfDigits(int newNumber)
+    {
+        int count;
+        for (count = 0; number > 1; count++)
+        {
+            number = number / 10;
+        }
+        return count;
+    }
+
+    int result = GetNumberOfDigits(newNumber);
+    
+    if (result == 1)
+    {
+        Console.WriteLine($"There is 1 digit");
+    }
+    else
+    {
+        Console.WriteLine($"There are {result} digits");
+    }
+}

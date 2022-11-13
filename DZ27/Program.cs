59a1ab9d531136ec43faa689db8d61b8665c1e11
Olga <olg3001@yandex.ru>
2022-11-14 01:02:14 +0300

    DZ27

---
+++ DZ27/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе. Через строку решать нельзя.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int DigitsSum(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number % 10;
+        number /= 10;
+    }
+    return result;
+}
+
+Console.WriteLine("Programm to get the digits sum of the number.");
+Console.WriteLine("Input number: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number >= 0)
+{
+    DigitsSum(number);
+}
+else
+{
+    number = number * -1;
+    DigitsSum(number);
+}
+
+Console.WriteLine("The digits sum of the number is " + DigitsSum(number));
